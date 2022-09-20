396ab344b3e16690550474d960cd2a3766e04951
Barchuk Andrii <abarchuk1976@gmail.com>
2022-09-20 23:03:17 +0300

    Fixed first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,72 @@
+const btnStartRef = document.querySelector('[data-start]');
+const btnStopRef = document.querySelector('[data-stop]');
 
+const TIMING = 1000;
+let timer;
+
+function toggleBtn(started) {
+  started
+    ? btnStopRef.removeAttribute('disabled')
+    : btnStopRef.setAttribute('disabled', true);
+
+  started
+    ? btnStartRef.setAttribute('disabled', true)
+    : btnStartRef.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function setColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function changeColor() {
+  timer = setInterval(setColor, TIMING);
+}
+
+function removeTimer() {
+  clearInterval(timer);
+}
+
+window.addEventListener('load', () => {
+  removeTimer();
+  const started = false;
+
+  toggleBtn(started);
+});
+
+btnStartRef.addEventListener('click', () => {
+  const started = true;
+
+  toggleBtn(started);
+  removeTimer();
+  changeColor();
+});
+
+document.addEventListener('keydown', event => {
+  if (event.code === 'Enter') {
+    const started = true;
+
+    toggleBtn(started);
+    removeTimer();
+    changeColor();
+  }
+});
+
+btnStopRef.addEventListener('click', () => {
+  const started = false;
+
+  toggleBtn(started);
+  removeTimer();
+});
+
+document.addEventListener('keydown', event => {
+  if (event.code === 'Escape') {
+    const started = false;
+
+    toggleBtn(started);
+    removeTimer();
+  }
+});
